b832ed9d229601f58d8237de706c307f3281d0e8
igorskyflyer <igor.dvlpr@gmail.com>
2023-04-03 16:59:28 +0200

    Update JSDocs

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
 
 /**
  * Strips only the HTML code while keeping the text content.
- * @param html the HTML string to process
- * @returns the processed string
+ * @param {string} html the HTML string to process
+ * @returns {string} the processed string
  */
 export function stripHtmlCode(html: string): string {
   return html.length === 0 ? '' : html.replace(/<[^>]+>([^<]+)?/gm, '$1').trim()
@@ -11,8 +11,8 @@ export function stripHtmlCode(html: string): string {
 
 /**
  * Strips HTML tags completely.
- * @param html the HTML string to process
- * @returns the processed string
+ * @param {string} html the HTML string to process
+ * @returns {string} the processed string
  */
 export function stripHtml(html: string): string {
   return html.length === 0 ? '' : html.replace(/<[^>]+>(?:[^<]+)?/gm, '').trim()
