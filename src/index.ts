abd133aed56eb16824d4612b9615a2185f7ab5d2
igorskyflyer <igor.dvlpr@gmail.com>
2023-04-03 15:18:27 +0200

    Add API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+// @igorskyflyer
 
+export function stripHtmlCode(html: string): string {
+  return html.length === 0 ? '' : html.replace(/<[^>]+>([^<]+)?/gm, '$1').trim()
+}
+
+export function stripHtml(html: string): string {
+  return html.length === 0 ? '' : html.replace(/<[^>]+>(?:[^<]+)?/gm, '').trim()
+}
