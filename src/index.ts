05e70002cdff82397cc35c4127c8db42a98ed0a7
igorskyflyer <igor.dvlpr@gmail.com>
2023-04-03 16:49:33 +0200

    Add JSDocs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 // @igorskyflyer
 
+/**
+ * Strips HTML tags completely.
+ * @param html the HTML string to process
+ * @returns the processed string
+ */
 export function stripHtmlCode(html: string): string {
   return html.length === 0 ? '' : html.replace(/<[^>]+>([^<]+)?/gm, '$1').trim()
 }
 
+/**
+ * Strips only the HTML code while keeping the text content.
+ *  @param html the HTML string to process
+ * @returns the processed string
+ */
 export function stripHtml(html: string): string {
   return html.length === 0 ? '' : html.replace(/<[^>]+>(?:[^<]+)?/gm, '').trim()
 }
